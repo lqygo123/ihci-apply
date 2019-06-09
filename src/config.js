6018abf109024a7e6ba990af1edad4c12086df4d
lu.qianyu <lqygo1234@qq.com>
2019-06-09 08:41:17 +0800

    feat: first version

---
+++ src/config.js
@@ -0,0 +1,37 @@
+export const CITY_LIST = [
+  '地区',
+  '广州',
+  '中山',
+  '佛山',
+  '地区',
+  '珠海',
+  '增城'
+]
+
+export const GRADE_TYPE = ['全年级', '小学', '初中', '高中']
+export const GRADE_GROUP_ALL = ['年级筛选']
+export const GRADE_GROUP_ELEMENTRAY = [
+  '小学组',
+  '一年级',
+  '二年级',
+  '三年级',
+  '四年级',
+  '五年级',
+  '六年级'
+]
+export const GRADE_GROUP_MIDDLE = ['初中组', '七年级', '八年级', '九年级']
+export const GRADE_GROUP_HIGH = [
+  '高中组',
+  '高中一年级',
+  '高中二年级',
+  '高中三年级'
+]
+
+export const SUBJECT_TYPE = ['全部', 'EV3', 'WeDo', 'scratch']
+export const SUBJECT_GROUP_ALL = ['科目']
+export const SUBJECT_GROUP_EV3 = ['全部', '初级', '中级', '高级']
+export const SUBJECT_GROUP_WEDO = ['全部', '初级', '中级', '高级']
+export const SUBJECT_GROUP_SCRATCH = ['全部', '初级', '中级', '高级']
+
+export const TERM_TYPE = ['学期', '春季班', '暑假班', '秋季班', '寒假班']
+export const COURSE_TYPE = ['课程类型', '常规课', '体验课']
